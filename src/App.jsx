0f52c05ac2bb82fa2a4fc9b81bdcf633231f7a55
Trapti Sharma <trapti1212sharma@gmail.com>
2025-12-29 11:34:48 +0530

    Home page Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 
 /* ===================== HOME ===================== */
-import Home from "./components/pages/home";
+import Home from "./components/pages/Home";
 
 /* ===================== PRODUCTS ===================== */
 
